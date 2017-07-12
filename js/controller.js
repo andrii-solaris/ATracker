5f4cdac79aed32b8938c3a40e0659ee78910cf63
Andrii <stellaralnilam@gmail.com>
2017-07-12 19:50:06 +0300

    Add files via upload
    - Some new Angular features. Still need to work on them.

---
+++ js/controller.js
@@ -0,0 +1,23 @@
+var studlist = angular.module('students-list', []);
+studlist.controller ('students-controller', function($scope){
+	$scope.records = [{name:'John',surname:'Doe',class:'primary'},{name:'Joe',surname:'Bloggs',class:'success'}, 
+            		{name:'John Q.',surname:'Doe', class:'purple'}, {name:'Mary',surname:'Major', class:'info'}, 
+          			{name:'Richard',surname:'Miles', class:'warning'}, {name:'Rudolf',surname:'Lingers',class:'danger'},
+            		{name:'Alan',surname:'Smithee',class:'primary'}, {name:'Luther',surname:'Blissett',class:'success'}, 
+           			{name:'Tommy',surname:'Atkins',class:'purple'}, {name:'Richmond',surname:'Way',class:'info'}, 
+           			{name:'Ron',surname:'Sanders',class:'warning'}, {name:'Megan',surname:'Rothmans',class:'danger'},
+           			{name:'Tom',surname:'Forest',class:'warning'}, {name:'Rachel',surname:'Stands',class:'danger'},
+           			{name:'Walter',surname:'Seems',class:'warning'}, {name:'Peter',surname:'Yang',class:'danger'},
+           			{name:'Ruth',surname:'Watkins',class:'warning'}, {name:'Melvin',surname:'Peters',class:'danger'}];
+});
+
+var login = angular.module('login-page', ['ngRoute']);
+login.controller ('login-controller', function($scope){
+	$scope.submit = function(){
+		var email = $scope.email;
+		var pwd = $scope.password;
+		if($scope.email == 'example@gmail.com' && $scope.password == '123456' ){
+			$location.path('/group-choice.html');
+		}
+	};
+});
