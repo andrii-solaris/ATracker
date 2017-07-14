28c60fba6fb4adc8bfc51a389783ec0f95a80f29
Andrii <stellaralnilam@gmail.com>
2017-07-14 04:06:01 +0300

    Add files via upload
    - Worked on jQuery and Angular features

--- js/controller.js
+++ js/controller.js
@@ -11,17 +11,17 @@ studlist.controller ('students-controller', function($scope){
            			{name:'Ruth',surname:'Watkins',class:'warning'}, {name:'Melvin',surname:'Peters',class:'danger'}];
 });
 
-var login = angular.module('login-page', []);
+var login = angular.module('login-page', ["ui.bootstrap.modal"]);
 login.controller ('login-controller', function($scope, $window){
 	$scope.submit = function(){
-		var email = $scope.email;
-		var pwd = $scope.password;
 		if($scope.email == 'example@gmail.com' && $scope.password == '123456' ){
-			 $window.location.href = 'group-choice.html'}
+			 $window.location.href = 'group-choice.html'
+			}
 		else { 
-			$(".btn-submit").click(function(){
-			    $(".btn-submit").attr("data-toggle", "modal");
-			});
+			$scope.ErrorAlert = true;					
 		};
 	};
+		$scope.cancel = function() {
+  			$scope.ErrorAlert = false;
+		};
 });
--- js/login-form.js
+++ js/login-form.js
@@ -28,4 +28,104 @@ $(document).ready(function(){
     		$(".p-glyph").hide();
     	}
 	});
+
+    $("#1").click(function(){
+        var existing = $("input:text").val();
+       if (existing.length < 4) {
+             $("input:text").val(existing + $("#1").attr("name"));
+        }
+        if (existing.length == 3) {
+            $(".dial-btn").addClass("disabled");
+        }
+    });
+    
+    $("#2").click(function(){
+        var existing = $("input:text").val();
+        if (existing.length < 4) {
+             $("input:text").val(existing + $("#2").attr("name"));
+        }
+        if (existing.length == 3) {
+            $(".dial-btn").addClass("disabled");
+        }
+    });
+
+    $("#3").click(function(){
+       var existing = $("input:text").val();
+       if (existing.length < 4) {
+            $("input:text").val(existing + $("#3").attr("name"));
+        }
+        if (existing.length == 3) {
+            $(".dial-btn").addClass("disabled");
+        }
+    });
+    
+    $("#4").click(function(){
+        var existing = $("input:text").val();
+        if (existing.length < 4) {
+             $("input:text").val(existing + $("#4").attr("name"));
+        }
+        if (existing.length == 3) {
+            $(".dial-btn").addClass("disabled");
+        }
+    });
+
+    $("#5").click(function(){
+       var existing = $("input:text").val();
+       if (existing.length < 4) {
+            $("input:text").val(existing + $("#5").attr("name"));
+       }
+       if (existing.length == 3) {
+            $(".dial-btn").addClass("disabled");
+        }
+    });
+    
+    $("#6").click(function(){
+        var existing = $("input:text").val();
+        if (existing.length < 4) {
+            $("input:text").val(existing + $("#6").attr("name"));
+        }
+        if (existing.length == 3) {
+            $(".dial-btn").addClass("disabled");
+        }
+    });
+
+    $("#7").click(function(){
+       var existing = $("input:text").val();
+       if (existing.length < 4) {
+            $("input:text").val(existing + $("#7").attr("name"));
+       }
+       if (existing.length == 3) {
+            $(".dial-btn").addClass("disabled");
+        }
+    });
+    
+    $("#8").click(function(){
+        var existing = $("input:text").val();
+        if (existing.length < 4) {
+            $("input:text").val(existing + $("#8").attr("name"));
+        }
+        if (existing.length == 3) {
+            $(".dial-btn").addClass("disabled");
+        }
+    });
+
+    $("#9").click(function(){
+       var existing = $("input:text").val();
+       if (existing.length < 4) {
+            $("input:text").val(existing + $("#9").attr("name"));
+       }
+       if (existing.length == 3) {
+            $(".dial-btn").addClass("disabled");
+        }
+    });
+    
+    $("#0").click(function(){
+        var existing = $("input:text").val();
+        if (existing.length < 4) {
+            $("input:text").val(existing + $("#0").attr("name"));
+        }
+        if (existing.length == 3) {
+            $(".dial-btn").addClass("disabled");
+        }
+    });
 });
