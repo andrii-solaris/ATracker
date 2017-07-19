3d09250328f687082c2bbe866b3c736f8bc06de1
Andrii <stellaralnilam@gmail.com>
2017-07-20 02:12:51 +0300

    Add files via upload
    - Continued working on statistics page

--- js/controller.js
+++ js/controller.js
@@ -1,14 +1,14 @@
 var studlist = angular.module('students-list', []);
 studlist.controller ('students-controller', function($scope){
-	$scope.records = [{number:'1',name:'John',surname:'Doe',class:'primary', status:'AS'},{number:'2',name:'Joe',surname:'Bloggs',class:'success',status:'AS'},
-            		{number:'3',name:'John Q.',surname:'Doe', class:'purple', status:'AS'}, {number:'4',name:'Mary',surname:'Major', class:'info', status:'AFS'},
-          			{number:'5',name:'Richard',surname:'Miles', class:'warning', status:'AFS'}, {number:'6',name:'Rudolf',surname:'Lingers',class:'danger', status:'AS'},
-            		{number:'7',name:'Alan',surname:'Smithee',class:'primary', status:'AFS'}, {number:'8',name:'Luther',surname:'Blissett',class:'success', status:'AS'},
-           			{number:'9',name:'Tommy',surname:'Atkins',class:'purple', status:'AS'}, {number:'10',name:'Richmond',surname:'Way',class:'info', status:'AS'},
-           			{number:'11',name:'Ron',surname:'Sanders',class:'warning', status:'AFS'}, {number:'12',name:'Megan',surname:'Rothmans',class:'danger', status:'AS'},
-           			{number:'13',name:'Tom',surname:'Forest',class:'warning', status:'AS'}, {number:'14',name:'Rachel',surname:'Stands',class:'danger', status:'AS'},
-           			{number:'15',name:'Walter',surname:'Seems',class:'warning', status:'AFS'}, {number:'16',name:'Peter',surname:'Yang',class:'danger', status:'AS'},
-           			{number:'17',name:'Ruth',surname:'Watkins',class:'warning', status:'AFS'}, {number:'18',name:'Melvin',surname:'Peters',class:'danger', status:'AS'}];
+	$scope.records = [{number:'1',name:'John',surname:'Doe',class:'primary', status:'UA'},{number:'2',name:'Joe',surname:'Bloggs',class:'success',status:'UA'},
+            		{number:'3',name:'John Q.',surname:'Doe', class:'purple', status:'UA'}, {number:'4',name:'Mary',surname:'Major', class:'info', status:'UA'},
+          			{number:'5',name:'Richard',surname:'Miles', class:'warning', status:'UA'}, {number:'6',name:'Rudolf',surname:'Lingers',class:'danger', status:'UA'},
+            		{number:'7',name:'Alan',surname:'Smithee',class:'primary', status:'UA'}, {number:'8',name:'Luther',surname:'Blissett',class:'success', status:'UA'},
+           			{number:'9',name:'Tommy',surname:'Atkins',class:'purple', status:'UA'}, {number:'10',name:'Richmond',surname:'Way',class:'info', status:'UA'},
+           			{number:'11',name:'Ron',surname:'Sanders',class:'warning', status:'UA'}, {number:'12',name:'Megan',surname:'Rothmans',class:'danger', status:'UA'},
+           			{number:'13',name:'Tom',surname:'Forest',class:'warning', status:'UA'}, {number:'14',name:'Rachel',surname:'Stands',class:'danger', status:'UA'},
+           			{number:'15',name:'Walter',surname:'Seems',class:'warning', status:'UA'}, {number:'16',name:'Peter',surname:'Yang',class:'danger', status:'UA'},
+           			{number:'17',name:'Ruth',surname:'Watkins',class:'warning', status:'UA'}, {number:'18',name:'Melvin',surname:'Peters',class:'danger', status:'UA'}];
 });
 
 var login = angular.module('login-page', ["ui.bootstrap.modal"]);
