8463cd45cd82318bf093fafd020f5368d859ca25
Andrii <stellaralnilam@gmail.com>
2017-07-26 01:00:45 +0300

    Add files via upload
    - Now one can select groups from Group-choice

--- js/controller.js
+++ js/controller.js
@@ -1,9 +1,15 @@
 var studlist = angular.module('students-list', []);
-studlist.controller ('students-controller', function($scope, $http){
+studlist.controller ('students-controller', function($scope, $http, $window){
 	$http.get('http://127.0.0.1/ATracker/js/simple-database.json')
-	.then(function(response) {		
-		$scope.records = response.data.information.green;
+	.then(function(response) {
+		$scope.records = eval(localStorage.getItem("stGroup"));
 	});
+		$scope.return_to_group = function(){
+		$window.location.href = 'group-choice.html';
+	};
+		$scope.student_click = function(){
+			$window.location.href = "pin.html";			
+		}
 });
 
 var login = angular.module('login-page', ["ui.bootstrap.modal"]);
@@ -28,6 +34,26 @@ var groupChoice = angular.module('group-choice-app',[]);
 groupChoice.controller ('group-choice-controller', function ($scope, $window) {
 	$scope.orange_submit = function(){
 		$window.location.href = 'students.html';
-		localStorage.setItem("stGroup", "orange");
+		localStorage.setItem("stGroup", "response.data.information." + "orange");
+	}
+
+	$scope.green_submit = function(){
+		$window.location.href = 'students.html';
+		localStorage.setItem("stGroup", "response.data.information." + "green");
+	}
+
+	$scope.blue_submit = function(){
+		$window.location.href = 'students.html';
+		localStorage.setItem("stGroup", "response.data.information." + "blue");
+	}
+
+	$scope.purple_submit = function(){
+		$window.location.href = 'students.html';
+		localStorage.setItem("stGroup", "response.data.information." + "purple");
+	}
+
+	$scope.red_submit = function(){
+		$window.location.href = 'students.html';
+		localStorage.setItem("stGroup", "response.data.information." + "red");
 	}
 });
