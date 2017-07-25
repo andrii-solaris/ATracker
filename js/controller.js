7f47ac4976d93eec532700238be82ff63ec1edff
Andrii <stellaralnilam@gmail.com>
2017-07-25 20:42:12 +0300

    Add files via upload
    - Some angular features, still need to figure out some points.

--- js/controller.js
+++ js/controller.js
@@ -1,18 +1,16 @@
 var studlist = angular.module('students-list', []);
 studlist.controller ('students-controller', function($scope, $http){
 	$http.get('http://127.0.0.1/ATracker/js/simple-database.json')
-	.then(function(response) {
-		$scope.records = response.data.information;
+	.then(function(response) {		
+		$scope.records = response.data.information.green;
 	});
 });
 
-var login = angular.module('login-page', ['ui.bootstrap.modal', 'ngRoute']);
+var login = angular.module('login-page', ["ui.bootstrap.modal"]);
 login.controller ('login-controller', function($scope, $window){
 	$scope.submit = function(){
 		if($scope.email == 'example@gmail.com' && $scope.password == '123456' ){
-			$rootScope.email = $scope.email
-			$rootScope.password = $scope.password
-			$window.location.href = 'group-choice.html';
+			 $window.location.href = 'group-choice.html'
 		}
 		else if($scope.email == 'teacher@gmail.com' && $scope.password == '654321') {
 			$window.location.href = "statistics.html";
@@ -24,4 +22,12 @@ login.controller ('login-controller', function($scope, $window){
 		$scope.cancel = function() {
   			$('#ErrorAlert').modal('hide');
 		};
+});
+
+var groupChoice = angular.module('group-choice-app',[]);
+groupChoice.controller ('group-choice-controller', function ($scope, $window) {
+	$scope.orange_submit = function(){
+		$window.location.href = 'students.html';
+		localStorage.setItem("stGroup", "orange");
+	}
 });
